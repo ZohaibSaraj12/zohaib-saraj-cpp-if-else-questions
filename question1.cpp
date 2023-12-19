e473c2548351c64869f5bf326d292a7666e00a9e
ZohaibSaraj12 <zohaibsaraj10@gmail.com>
2023-12-19 09:47:51 +0500

    Create question1.cpp

---
+++ question1.cpp
@@ -0,0 +1,36 @@
+//sum of digits of a number using only devid statements 
+
+#include<iostream>
+using namespace std;
+int main(){
+int n1;
+cout<<"Enter any number = ";
+cin>>n1;
+//for 1st digit remainder 
+int digit1=n1%10;
+//for 1st digit quotient 
+n1=n1/10;
+int digit2=n1%10;
+n1=n1/10;
+int digit3=n1%10;
+n1=n1/10;
+
+int digit4=n1%10;
+n1=n1/10;
+int digit5=n1%10;
+n1=n1/10;
+int digit6=n1%10;
+n1=n1/10;
+int sum;
+sum=digit1+digit2+digit3+digit4+digit5+digit6;
+cout<<"The sum of digits of a number is = "<<sum;
+
+
+
+
+
+
+
+
+    return 0;
+}
