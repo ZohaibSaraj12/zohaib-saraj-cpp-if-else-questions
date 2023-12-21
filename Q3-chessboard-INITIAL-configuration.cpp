34dade6f05d6db92ee69a74423d12e37b935d740
Zohaib Saraj <154296659+ZohaibSaraj12@users.noreply.github.com>
2023-12-21 12:54:29 +0500

    Add files via upload

---
+++ Q3-chessboard-INITIAL-configuration.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+	/* AS we know chess board initial configuration means that we have 
+    8 rows and 8 columns in chess board as we 
+    know 1 player with small letters and second player with capital letters 
+    p means for pawn and r for "ROCK" n for "KNIGHT" b for "BISHOPS" q for "QUEEN" k for 
+    "KING". 
+    It is written as "r n b q k b n r" it is an initial configuration according to chess 
+    game p means for pawn it is initial configuration in chess board only understand 
+    what does 
+     */
+	
+cout<<" a b c d e f g h\n";
+cout<<"8 p p p p p p p p\n";
+cout<<"7 r n b q k b n r\n";
+cout<<"6 . . . . . . . .\n";
+cout<<"5 . . . . . . . .\n";
+cout<<"4 . . . . . . . .\n";
+cout<<"3 . . . . . . . .\n";
+cout<<"2 P P P P P P P P\n";
+cout<<"1 R N B Q K B N R\n";
+	
+	return 0;
+}
