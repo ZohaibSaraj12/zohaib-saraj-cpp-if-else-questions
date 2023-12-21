8773e28c7e7831b3c8358f9dc07e29cf2690afce
Zohaib Saraj <154296659+ZohaibSaraj12@users.noreply.github.com>
2023-12-21 13:32:15 +0500

    Add files via upload

---
+++ Q5-check-whether-no-is-multiple-of-other.cpp
@@ -0,0 +1,24 @@
+//check no is multiple of other number 
+#include<iostream>
+using namespace std;
+int main(){
+int n1;
+int n2;
+cout<<"Enter first no = ";
+cin>>n1;
+cout<<"Enter second no = ";
+cin>>n2;
+if(n1%n2==0)
+cout<<"Number is multiple of other "<<endl;
+
+else 
+cout<<"ERROR! Number is not the multiple of other number "<<endl;
+
+
+
+
+
+
+
+    return 0;
+}
