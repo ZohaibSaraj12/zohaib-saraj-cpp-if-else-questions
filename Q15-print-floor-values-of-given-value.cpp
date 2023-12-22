0acb535618326e73f011a7629c092bf5226be1ac
Zohaib Saraj <154296659+ZohaibSaraj12@users.noreply.github.com>
2023-12-22 20:42:36 +0500

    Add files via upload

---
+++ Q15-print-floor-values-of-given-value.cpp
@@ -0,0 +1,19 @@
+/*what is floor value means the floor value of a number is defined as the smallest or equal value of that integer.
+In other words, the floor function rounds down a real number to the nearest integer that is not greater than the 
+ original number. 
+ ⌊x⌋=the greatest integer n such that n≤x */
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+  double x;
+  cout<<"Enter number = ";
+  cin>>x;
+  
+
+  cout << floor(x);
+
+  return 0;
+}
+
