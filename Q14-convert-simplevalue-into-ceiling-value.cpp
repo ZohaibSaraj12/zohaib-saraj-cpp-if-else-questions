852e33ad8906a17a13e484da182be0c924232e72
Zohaib Saraj <154296659+ZohaibSaraj12@users.noreply.github.com>
2023-12-22 20:48:50 +0500

    Add files via upload

---
+++ Q14-convert-simplevalue-into-ceiling-value.cpp
@@ -0,0 +1,15 @@
+ /*In other words, the ceiling function rounds up a real number to the nearest integer that is not less 
+ than the original number.
+⌈x⌉=the smallest integer n such that n≥x */
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+	double n1;
+	cout<<"Enter value to convert in ceiling = ";
+	cin>>n1;
+	cout<<"Ceiling value of "<<n1<<" is "<<" = "<<ceil(n1)<<endl;
+
+
+	return 0;
+}
