85c2a7c0cc6c30c1a124d200c20d55ce4ceb15d8
Zohaib Saraj <154296659+ZohaibSaraj12@users.noreply.github.com>
2023-12-25 12:02:34 +0500

    Add files via upload

---
+++ hlozaib.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n1;
+	cout<<"Enter number 1 = ";
+	cin>>n1;
+	int n2;
+	cout<<"Enter number 2 = ";
+	cin>>n2;
+	 if (n1!=n2)
+	 {
+	 	int temp=n1;
+	 	n1=n2;
+	 	n2=temp;
+	 	
+	 	
+	 	
+	 	
+	 }
+	cout<<"First number after swapping is = "<<n1<<endl;
+	cout<<"Second number after swapping is = "<<n2<<endl;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
